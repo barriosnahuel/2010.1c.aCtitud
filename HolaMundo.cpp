6ddb95c31f51c4a1fa0b0cbbb60ca52b2371acce
Fernando Guerra <53754439+ferwguerra@users.noreply.github.com>
2010-04-21 15:35:07 +0000

    Sigo probando,...

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -10,14 +10,21 @@ main()
 {
 	vector<string> VectorDeComandos;
 
-
-	VectorDeComandos.push_back("List newsgroups");
+	VectorDeComandos[0] = "List newsgroups";
+	VectorDeComandos[1] = "Quit";
+	VectorDeComandos[2] = "Listgroup";
+	VectorDeComandos[3] = "Article";
+	VectorDeComandos[4] = "Stat";
+	VectorDeComandos[5] = "Head";
+	VectorDeComandos[6] = "Body";
+	
+	/*VectorDeComandos.push_back("List newsgroups");
 	VectorDeComandos.push_back("Quit");
 	VectorDeComandos.push_back("Listgroup");
 	VectorDeComandos.push_back("Article");
 	VectorDeComandos.push_back("Stat");
 	VectorDeComandos.push_back("Head");
-	VectorDeComandos.push_back("Body");
+	VectorDeComandos.push_back("Body");*/
 
    cout << "Mostrando los comandos:" << endl;
 
