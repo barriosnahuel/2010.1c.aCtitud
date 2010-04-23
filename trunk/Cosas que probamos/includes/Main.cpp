a9b54f804ff71b8750b07a9ea6c407fe6c8c5a34
1346880.barrios.nahuel <1346880.barrios.nahuel@9a0c02b7-1ca9-40be-a75d-8383f3ce0902>
2010-04-23 03:42:06 +0000

    NBarrios: Ejemplo andando de como hacer los includes, definir clases, etc.

--- trunk/Cosas que probamos/includes/Main.cpp
+++ trunk/Cosas que probamos/includes/Main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include "Comando.hpp"
+#include "Validator.h"
 using namespace std;
 
 int main(void){
@@ -16,6 +17,11 @@ int main(void){
 	comando.setComando("mira vos che!");
 
 	cout << comando.getComando() << endl;
+
+	cout << "ahora uso la clase Validator" << endl;
+
+	Validator unValidator;
+	cout << unValidator.validameEsta(2) << endl;
 
 	cout << "Ending..." << endl;
 	return 0;
