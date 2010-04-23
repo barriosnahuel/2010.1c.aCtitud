6948fef466254ff86109dc60181f227bd325a2b3
Fernando Guerra <53754439+ferwguerra@users.noreply.github.com>
2010-04-23 02:12:02 +0000

    Subo comando.cpp compilando!!

--- NNTPClient/comando.cpp
+++ NNTPClient/comando.cpp
@@ -9,13 +9,10 @@ class Comando
             string nombreComando;
             //Esto es 1 o 0 si el comando lleva parametro. Por ej para "Article 321", esto iria en 1.
             int llevaParametro;
+
     public:
             //Constructor
             Comando comando(void);
-
-
-            /* Los getters y settes los implemento directamente acá por cuestiones de comodidad y prolojidad. Según el ejemplo
-            que estoy tomando, se puede... */
 
             string getNombreComando(void){
                 return nombreComando;
@@ -29,7 +26,7 @@ class Comando
                 nombreComando = nombreDelComando;
             }
 
-            void setllevaParametro(int valor){
+            void setLlevaParametro(int valor){
                 llevaParametro = valor;
             }
-}
+};
