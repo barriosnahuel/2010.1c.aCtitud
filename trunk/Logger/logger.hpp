9bbad48f6d995bb5a8e011e5ee02a30172a28290
1153500.Malsam.Leandro <1153500.Malsam.Leandro@9a0c02b7-1ca9-40be-a75d-8383f3ce0902>
2010-04-23 01:43:31 +0000

    logger de lilnux cpp v1.0

---
+++ trunk/Logger/logger.hpp
@@ -0,0 +1,13 @@
+#ifndef _LOGGER_HPP
+
+#define STDIN 0
+
+#define _INFO 1
+#define _WARN 2 
+#define _ERROR 3
+#define _DEBUG 4
+
+    public:
+       void EscribirLog(char cTipoLog, char *czData, char *czNomProc);
+
+#endif  /* _LOGGER_HPP */
