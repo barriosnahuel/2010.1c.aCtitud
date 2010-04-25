811743ee13871993f31290ae8267bc25b22abafc
1338237.guerra.fernando <1338237.guerra.fernando@9a0c02b7-1ca9-40be-a75d-8383f3ce0902>
2010-04-25 19:54:41 +0000

    Arreglo de inicializarDAO

--- trunk/NNTPClient/nntpClient.cpp
+++ trunk/NNTPClient/nntpClient.cpp
@@ -9,7 +9,6 @@
 #include <pthread.h>
 #include "Comando.hpp"
 #include "ParseadorValidadorDeComandos.h"
-#include "NNTPClientDAO.h"
 #include <stdlib.h>
 #include "Semaforo.h"
 #include "NNTPClientDAO.hpp"
