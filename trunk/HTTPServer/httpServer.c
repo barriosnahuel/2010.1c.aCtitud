b3ad46e532c649ffa15590174fb529aff7a63a19
1346880.barrios.nahuel <1346880.barrios.nahuel@9a0c02b7-1ca9-40be-a75d-8383f3ce0902>
2010-04-29 02:59:43 +0000

    NBarrios: Actualización.

--- trunk/HTTPServer/httpServer.c
+++ trunk/HTTPServer/httpServer.c
@@ -6,27 +6,28 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <thread.h>
 
-#define PORT 15000 /* El puerto que será abierto */
-#define BACKLOG 3 /* El número de conexiones permitidas */
+#define PORT 15000 /* El puerto que serï¿½ abierto */
+#define BACKLOG 3 /* El nï¿½mero de conexiones permitidas */ //	TODO: Aca no tendriamos que poner por lo menos 20?
 
-//void* gestionarSolicitudDelClienteThread(void* parametro) {
-//	if (/*//	TODO: No esta en el cache*/true) {
-//	TODO: Lo busco en la DB.
+/**
+ * Nota: Para Solaris el header se llama asi (thread.h). A diferencia de pthread, los hilos pueden ser gestionados
+ * por el kernel directamente.
+ */
 
-//	TODO: Guardo en cache.
 
-//	TODO: Formateo la respuesta a HTML.
+//	---------------------------
+//	Declaraciones de funciones.
 
-//	TODO: Devuelvo la respuesta. (Ver como hacer esto, que paremetro es "parametro".. capaz tengo que pasar el socket como parametro para asi poder responderle!!)
+void* procesarRequestFuncionThread(void* parametro);
+int crearSocketYDejarEscuchando(int ficheroServer,
+		struct sockaddr_in server, int sin_size);
 
-//	TODO: Termino el thread. (Chequear si no hay que hacer algo con el socket, o el medio por el cual devuelvo la rta).
-//	}
+//	--------------------------
+//	Definiciones de funciones.
 
-//	return parametro;
-//}
-
-int crearSocketYDejarEscuchando(int ficheroServer, int ficheroCliente,
+int crearSocketYDejarEscuchando(int ficheroServer,
 		struct sockaddr_in server, struct sockaddr_in client, int sin_size) {
 	if ((ficheroServer = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
 		printf("Error al crear el socket.\n");
@@ -34,7 +35,7 @@ int crearSocketYDejarEscuchando(int ficheroServer, int ficheroCliente,
 	}
 	printf("Socket creado.\n");
 	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY; /* INADDR_ANY coloca nuestra dirección IP automáticamente */
+	server.sin_addr.s_addr = INADDR_ANY; /* INADDR_ANY coloca nuestra direcciï¿½n IP automï¿½ticamente */
 	server.sin_port = htons(PORT); /* htons transforma el short de maquina a short de red */
 	bzero(&(server.sin_zero), 8); /* escribimos ceros en el reto de la estructura*/
 
@@ -50,39 +51,110 @@ int crearSocketYDejarEscuchando(int ficheroServer, int ficheroCliente,
 	}
 	printf("Escuchando conexiones en el puerto %d.\n", PORT);
 
+
+
+	//	TODO: Segun lo que entiendo de la Guia Beej, primero tengo que aceptar, y despues crear el hilo. Con eso alcanza porque
+	//			a la funcion le paso el fd y ya puedo hacer el send() que necesito para responderle al cliente.
+
+
+	//	Aca creo un while para crear threads y aceptar conexiones.
+
+	//	sin_size "Es un puntero a una variable local int a la que deberÃ­as asignar el valor de sizeof(struct sockaddr_in)".
+	//	Joya, pero que carajo es?
 	sin_size = sizeof(struct sockaddr_in);
-	if ((ficheroCliente = accept(ficheroServer, (struct sockaddr *) &client,
-			&sin_size)) == -1) {
-		printf("Error al aceptar conexión.\n");
-		return -1;
+
+	while(true){	//	NBarrios-TODO: Check this!
+		struct sockaddr_in client; /* para la informaciï¿½n de la direcciï¿½n del cliente */
+
+		//	1. Obtengo una conexion que esta pendiente en la cola de entrada, y obtengo un descriptor de fichero de socket nuevo
+		//	exclusivo de esa conexion.
+		//	2. Declaro recien en este momento a ficheroCliente, porque preciso uno por cada conexion. Estaba declarado en el main.
+		int ficheroCliente= accept(ficheroServer, (struct sockaddr *) &client, &sin_size);
+		if(ficheroCliente!=-1){
+			//	Si no hubo errores aceptando la conexion, entonces la gestiono.
+
+			pthread_t threadProcesarRequest;//	Declaro un nuevo thread.
+			//	NBarrios-TODO: Seteo todo lo que tenga que setearle al thread, si es que hay que setearle algo.
+
+			//	En Solaris!!
+			//	NBarrios-TODO: Ver como se usa esta funcion!!
+			//	El formato es:
+//								int thr_create(void *stack_base
+//													, size_t stack_size
+//													, void *(*start_routine) (void *)
+//													, void *arg
+//													, long flags
+//													, thread_t *new_thread
+//												);
+//			thr_create(, , &procesarRequestFuncionThread, , , threadProcesarRequest);
+
+
+			//	En Debian seria asi:
+//			pthread_create(&threadProcesarRequest, NULL, &procesarRequestFuncionThread, (void*)ficheroCliente);
+
+
+			printf("Se obtuvo una conexion desde %s...\n", inet_ntoa(client.sin_addr));
+		}
+		else
+			printf("No se pudo aceptar la conexion.");
 	}
-	printf("Se obtuvo una conexión desde %s...\n", inet_ntoa(client.sin_addr));
+
+
+
+
+	//	Esto es lo que estaba!!
+//	sin_size = sizeof(struct sockaddr_in);
+//	if ((ficheroCliente = accept(ficheroServer, (struct sockaddr *) &client,
+//			&sin_size)) == -1) {
+//		printf("Error al aceptar conexiï¿½n.\n");
+//		return -1;
+//	}
+
+//	printf("Se obtuvo una conexion desde %s...\n", inet_ntoa(client.sin_addr));
 	return 1;
 }
 
-int main(void) {
-	int ficheroServer, ficheroCliente; /* los ficheros descriptores */
-	int sin_size;
-	struct sockaddr_in server; /* para la información de la dirección del servidor */
-	struct sockaddr_in client; /* para la información de la dirección del cliente */
+/**
+ * Esta funcion es el "main" del thread que se encarga de procesar el request.
+ */
+void* procesarRequestFuncionThread(void* parametro){
+	int* ficheroCliente= (int*)parametro;
+	//	Con este fichero deberia alcanzar para poder responderle al cliente. En realidad esto lo estamos analizando.
 
-	//	TODO: Crear un socket para escuchar las peticiones que van a venir del browser.
-	crearSocketYDejarEscuchando(ficheroServer, ficheroCliente, server, client,
-			sin_size);
+	String response= null;
+//	if (/*//	TODO: No esta en el cache*/) {
+		//	TODO: Lo busco en la DB.
+
+		//	TODO: Guardo en cache.
+//	}
+
+	//	Para este momento ya tengo el response seteado.
+
+	//	TODO: Formateo la respuesta a HTML.
+
+	//	TODO: Devuelvo la respuesta por medio del "send(*ficheroCliente,...)" (Ver si en Solaris se llama igual)
+
+	close(*ficheroCliente); //	NBarrios-TODO: Chequear si se llama igual en Solaris! (Estoy cerrando el socket).
+
+	//	TODO: Termino el thread.
+
+	return parametro;	//	NBarrios-TODO: Check this!
+}
+
+int main(void) {
+	int ficheroServer; /* los ficheros descriptores */
+	int sin_size;//	TODO: Esto hace falta declararlo aca? Que es?
+	struct sockaddr_in server; /* para la informaciï¿½n de la direcciï¿½n del servidor */
 
 	//	TODO: Conectarme a la DB openDS.
 
-	//while (true) {//	While true porque tengo que escuchar todo el tiempo conexiones entrantes. En realidad hay que ver como cerrar el programa de una manera "linda".
-	//	TODO: Acepto una conexion nueva.
+	//	TODO: Crear un socket para escuchar las peticiones que van a venir del browser.
+	crearSocketYDejarEscuchando(ficheroServer, ficheroCliente, server, sin_size);
 
-	//	TODO: Creo un nuevo thread para atender esa conexion. (Seria la funcion: gestionarSolicitudDelClienteThread)
-	//}
-
-	//	TODO: Me desconecto de la DB.
+	//	TODO: Me desconecto de la DB, pero una vez que cerre el socket que esta escuchando. Ver como averiguar eso.
 
 	printf("Te pudiste conectar amigowww!!!\n");
 	printf("Estoy por cerrar los sockets\n");
-	close(ficheroCliente);
 	close(ficheroServer);
 
 	//	TODO: Libero la lo ultimo que pueda llegar a quedar de memoria pedida.
