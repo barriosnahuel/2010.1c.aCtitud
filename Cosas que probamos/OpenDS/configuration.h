82ace4ab82f845e52b74c9955b94a8fbe8a673f3
Nahuel Barrios <barrios.nahuel@gmail.com>
2010-05-07 01:53:04 +0000

    NBarrios: OpenDs con archivos de configuracion integrado. Hay que hacer andar el LDAPWrapper.

---
+++ Cosas que probamos/OpenDS/configuration.h
@@ -0,0 +1,30 @@
+#ifndef _CONFIGURATION_H
+#define _CONFIGURATION_H
+
+/*	Estas constantes son las que mapeo con el archivo de configuracion.	*/
+#define APP_PORT "AppPort="
+#define OPENDS_SERVER "OpenDSServer="
+#define OPENDS_PORT "OpenDSPort="
+
+
+/* estructura que guarda la configuracion
+ * se debe modificar acorde a los datos
+ * que se tomaran del archivo */
+typedef struct stConfiguracion {
+    unsigned int uiAppPuerto;	/*	Puerto sobre el que estara escuchando la aplicacion.	*/
+
+	char czBDServer[15+1];  	/* se supone que es una direccion IP */
+    unsigned int uiBDPuerto;
+} stConfiguracion;
+
+/* funcion que valida que una ip se haya ingresado correctamente */
+int Valida_IP(const char *ip);
+
+/* funcion que toma un valor del archivo del configuracion */
+char *GetVal(const char *sValBuff, const char *sBuff);
+
+/* funcion que carga el archivo indicado. se debe modificar acorde
+ * a la configuracion que se desee cargar */
+int CargaConfiguracion(char *pszNombreArchivo, stConfiguracion *stConf);
+
+#endif
