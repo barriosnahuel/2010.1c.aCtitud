d5cff7a829fb5f28e0f8217aa6bc37b019c728ec
1338237.guerra.fernando <1338237.guerra.fernando@9a0c02b7-1ca9-40be-a75d-8383f3ce0902>
2010-05-12 01:12:09 +0000

    Probando parseo

---
+++ trunk/Cosas que probamos/Fer/PruebaParseo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+char* obtenerRecursoDeCabecera(char* sMensajeHTTPCliente);
+char* sacarEspacios(char* sMensajeHTTPCliente);
+
+int main() {
+	char cadenaAParsearDePrueba[1024] = "GET / HTTP/1.1\n";
+	char recursoObtenido[1024];
+	
+	printf("La cadena a parsear es: %s", recursoObtenido);
+	recursoObtenido = obtenerRecursoDeCabecera(cadenaAParsearDePrueba);
+	printf("El recurso obtenido es: %s", recursoObtenido);
+	return 1;
+	
+	
+}
+
+char* obtenerRecursoDeCabecera(char* sMensajeHTTPCliente) {
+	int i = 0;
+	int j = 0;
+	char recurso[1024];
+	
+	while(sMensajeHTTPCliente[i] != '/') {
+		i = i + 1;
+	}
+	
+	while(sMensajeHTTPCliente[i] != '.') {
+		recurso[j] = sMensajeHTTPCliente[i];
+		i = i + 1;
+		j = j + 1;
+	}
+	recurso[j] = '\n';
+	return recurso;
+	
+}
