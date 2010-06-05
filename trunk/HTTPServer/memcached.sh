2ee23c533f6bd5867cac03f7efc5c6d897d5b0e6
1346880.barrios.nahuel <1346880.barrios.nahuel@9a0c02b7-1ca9-40be-a75d-8383f3ce0902>
2010-06-05 02:42:49 +0000

    NBarrios: Actualizacion, cree memcached.sh para levantar memcached usando sh memcached.sh start ip puerto.

---
+++ trunk/HTTPServer/memcached.sh
@@ -0,0 +1,39 @@
+# Especifico una constante, que es el path absoluto del directorio donde esta el OopenDS
+
+# Hago un case con el parametro 1, para ver que quiero hacer con el server.
+case "${1}" in
+	start)
+		echo "Iniciando Memcached..."
+		"/usr/lib/memcached -l ${2} -p ${3} -d -u root"
+		exit ${?}
+		;;
+		
+	install)
+		echo "Instalando Memcached..."
+		echo "Nota: El install no fue probado."
+		"wget http://www.monkey.org/%7Eprovos/libevent-1.4.13-stable.tar.gz"
+		"gtar -xvzf libevent-1.4.13-stable.tar.gz"
+	
+		"wget http://memcached.googlecode.com/files/memcached-1.4.5.tar.gz" 
+		"gtar -xvzf memcached-1.4.5.tar.gz"
+	
+		"su"
+		"osol0906"
+
+		"cd libevent-1.4.13-stable"
+		"./configure" 
+		"make"
+		"make install"
+		
+		"cd memcached-1.4.5"
+		"./configure"
+		"make"
+		"make install"
+		exit ${?}
+		;;		
+	  
+	*)
+		echo "Los comandos disponibles son:  $0 { start ip port | install }"
+		exit 1
+		;;
+esac
